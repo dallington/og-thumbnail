3aa722ae190c0bac1518ff284ff84a90a48accb8
Dallington Augusto <contato@dallington.com.br>
2021-07-24 19:53:00 -0300

    add index in pages

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>oi index</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Logo from "~/components/Logo.vue";
+export default Vue.extend({
+  components: {
+    Logo
+  },
+
+  data() {
+    const message: string = "This is a message";
+    return {
+      message
+    };
+  }
+});
+</script>
